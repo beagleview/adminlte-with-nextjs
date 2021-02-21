e513c30a1550c87e8625ba1fda018a76892fce92
taechitc <beagleview@outlook.com>
2021-02-21 23:59:21 +0700

    add header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import AdminHeader from "../components/layouts/admin.header";
 
 export default () => {
     return (
         <div>
+            <AdminHeader />
             <h1>Hello Dek Dek</h1>
         </div>
     )
