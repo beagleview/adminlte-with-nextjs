25bdf501b7c3e8bc5ae11dc977e28ca9ed368582
taechit.cha <taechit.cha@ascendcorp.com>
2021-04-15 14:35:36 +0700

    update document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,19 @@
-import Document, {Html, Head, Main, NextScript} from "next/document";
+import Document, {Html, Head, Main, NextScript, DocumentContext} from "next/document";
 
 export default class DocumentTemplate extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
     render() {
         return (
             <Html>
                 <Head>
                     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"/>
                     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
+                    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css"/>
                     <link rel="stylesheet" href="dist/css/adminlte.min.css"/>
                     <link rel="stylesheet"
                           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700"/>
